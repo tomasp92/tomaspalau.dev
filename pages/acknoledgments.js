904cf590a40c18ab98a0a4d51a889e8862838a63
tomasp92 <tomas@atexto.com>
2022-08-31 16:13:38 -0300

    add harvard acknoledgment

--- pages/acknoledgments.js
+++ pages/acknoledgments.js
@@ -1,7 +1,25 @@
 export default function Acknoledgments() {
     return(
-        <div>
-            <h1>Acknoledgments</h1>
-        </div>
+        <>
+            <div>
+                <h1>Acknoledgments</h1>
+                <div id="harvard">
+                    <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:6775933718940004352" height="714" width="704" frameBorder="0" allowFullScreen="true" title="Publicación integrada"></iframe>
+                </div> 
+            </div>
+            <style jsx>{`
+                #harvard{
+                    margin: auto;
+                    padding: auto;
+                    text-align: center;
+                    border: solid 3px grey;
+                }
+                iframe{
+                    margin: auto;
+                    padding: auto;
+                    text-align: center;
+                }
+            `}</style>
+        </>
     )
 }
