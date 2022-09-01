a5c9e5ef10b9a3a2b5323afc488a7d6c1123d597
tomasp92 <tomas@atexto.com>
2022-09-01 10:32:20 -0300

    add english certificate to acknoledgments

--- pages/acknoledgments.js
+++ pages/acknoledgments.js
@@ -1,14 +1,22 @@
 export default function Acknoledgments() {
     return(
         <>
-            <div>
+            <div className="acknoledgmentsContainer">
                 <h1>Acknoledgments</h1>
-                <div id="harvard">
+                <div className="iframeContainer">
                     <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:6775933718940004352" height="714" width="704" frameBorder="0" allowFullScreen="true" title="Publicación integrada"></iframe>
-                </div> 
+                </div>
+                <div className="iframeContainer">
+                    <iframe src="https://www.efset.org/cert/f1GeqH" height="714" width="704" frameBorder="0" allowFullScreen="true" title="Publicación integrada"></iframe>
+                </div>
             </div>
             <style jsx>{`
-                #harvard{
+                .acknoledgmentsContainer{
+                    margin: auto;
+                    padding: auto;
+                    text-align: center;
+                }
+                .iframeContainer{
                     margin: auto;
                     padding: auto;
                     text-align: center;
